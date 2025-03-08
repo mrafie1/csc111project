3f3d7804fb18eff2f41f1168d4955acf2e7e7e9b
mrafie1 <rafie.muhammad2007@gmail.com>
2025-03-08 11:42:52 -0500

    Updated the classes.py file
    Added _Player (_vertex class),
    _Connection (edge class),
    Teamgraph (graph)

--- classes.py
+++ classes.py
@@ -1,1 +1,34 @@
+from __future__ import annotations
+from typing import Any
 
+
+class _Player:
+    """ Player class representing the statistics of each player.
+    Analogous to the _vertex class in a graph.
+    """
+    name: str
+    avg_points: int
+    avg_rebounds: int
+    avg_assists: int
+    player_impact_estimate: int
+    # Add further statistics attributes
+
+
+class _Connection:
+    """ An 'edge' class connecting two _player classes and representing their shared statistics.
+    Representation Invariants:
+        - len(player_connection) == 2 or len(player_connection) == 0
+    """
+    player_connection: list[_Player]
+    # Add further statistic attributes
+
+
+class Teamgraph:
+    """ A graph whose vertices are _player class.
+    Representation Invariants:
+    - all(name == self._players[name].name for name in self._players)
+    """
+    # Private Instance Attributes:
+    #     - _players: A collection of the players contained in this graph.
+    #                  Maps name to _Player instance.
+    _players: dict[str, _Player]
