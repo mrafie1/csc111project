7267af9ffe0d8c8ae18afd288e31f10ad21afdb4
mrafie1 <rafie.muhammad2007@gmail.com>
2025-03-16 12:40:47 -0400

    Changed classes.py and main.py

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
 # MAIN FILE FOR PYTHON PROJECT
-print("Hello")
-print("World")
+from __future__ import annotations
+import json
+from typing import Optional
+
+from classes import Graph
+
+
+class display_stats:
+    """
+    sjiodnowsnodw
+    """
+
+    team_graph: Graph
+    best_team_graph: Graph
+
+    def __init__(self, data_file: str) -> None:
+        self.team_graph = self._load_game_data(data_file)
+
+    @staticmethod
+    def _load_game_data(filename: str) -> Optional[Graph]:
+        with open(filename, 'r') as f:
+            data = json.load(f)
+
+        # Adding players first
+        return None
