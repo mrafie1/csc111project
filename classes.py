a314777267ffc2687731298b619cda1cc7d94a01
houssamyaacoub <146137872+houssamyaacoub@users.noreply.github.com>
2025-03-23 12:33:23 -0400

    Update classes.py
    Copied from test1. Changes keys for _players dict to tuple

--- classes.py
+++ classes.py
@@ -37,10 +37,8 @@ class _Player:
         self.player_impact_estimate = 0
 
 
-
-
 class _Connection:
-    """ An 'edge' class connecting two _player classes and representing their shared statistics.
+    """ An 'edge' class connecting two _Player classes and representing their shared statistics.
     Representation Invariants:
         - len(player_connection) == 2 or len(player_connection) == 0
     """
@@ -70,8 +68,6 @@ class _Connection:
         self.max_avg_assists_per_pass = max(self.player1_avg_assists_per_pass, self.player2_avg_assists_per_pass)
 
 
-
-
 class Graph:
     """ A graph whose vertices are _player class.
     Representation Invariants:
@@ -81,18 +77,30 @@ class Graph:
     #     - _players: A collection of the players contained in this graph.
     #                  Maps name to _Player instance.
     _players: dict[str, _Player]
-    _connections: dict[set[str], _Connection]
+    _connections: dict[tuple[str, str], _Connection]
 
     def __init__(self):
         self._players = {}
+        self._connections = {}
 
     def add_player(self, player: _Player):
         if player.name not in self._players:
             self._players[player.name] = player
 
     def add_connection(self, player1: _Player, player2: str, assist, passes, minutes_together):
-        if {player1.name, player2} not in self._connections:
-            new_connection = _Connection(player1, self._players[player2])
-            self._connections[{player1.name, player2}] = new_connection
+        """
+        Adds connection between two players using the _Connection class
 
-        self._connections[{player1.name, player2}].tweak_stats(player1, assist, passes, minutes_together)
+        Representation Invariants:
+            - player2 in self._players
+        """
+        if (player1.name, player2) not in self._connections:
+            new_connection = _Connection(player1, self._players[player2])
+            self._connections[(player1.name, player2)] = new_connection
+
+        self._connections[(player1.name, player2)].tweak_stats(player1, assist, passes, minutes_together)
+
+    def check_exists(self, player1_name: str, player2_name: str) -> bool:
+
+        keys = self._players.keys()
+        return (player1_name, player2_name) in keys or (player1_name, player2_name) in keys
