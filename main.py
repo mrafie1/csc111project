290c2146edfa13f61a244640afbcb07e1170c9d2
houssamyaacoub <146137872+houssamyaacoub@users.noreply.github.com>
2025-03-23 12:13:03 -0400

    Update main.py
    Instantiated connections

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ class LineupSimulation:
     # Private Instance Attributes:
     #   -
 
-    players = set
+    players = {}
 
     def _load_game_data(self, filename: str) -> Graph:
         """Load players from a JSON file with the given filename and
@@ -26,37 +26,49 @@ class LineupSimulation:
         with open(filename, 'r') as f:
             data = json.load(f)  # This loads all the data from the JSON file
 
-        players = {}
         graph = Graph()
+
+        # Instanciating player objects
         for loc_data in data:
             passes = loc_data['passes_to']
             defense_stats = loc_data['defensive_stats']
-            assists = self.process_passes(passes)
+            assists, interactions = self.process_assists(passes)
             defense = self.process_defense(defense_stats)
 
             player_obj = _Player(loc_data['name'], loc_data['team'], loc_data['positions'], defense['points'],
                                  defense['rebounds'], assists, defense['minutes'],
                                  defense['steals'], defense['blocks'])
-            players[player_obj.name] = player_obj
+            graph.add_player(player_obj)
+            self.players[player_obj.name] = (player_obj, interactions)
 
-        graph._players = players
+        # graph._players = players
         # print(players)
+
+        for player in self.players:
+            player_interactions = self.players[player][1]
+
+            for player_name in self.players[player][1]:
+                assists = player_interactions[player_name]['assists']
+                total_passes = player_interactions[player_name]['total_passes']
+                minutes_together = player_interactions[player_name]['minutes_together']
+                graph.add_connection(self.players[player][0], player_name, assists, total_passes, minutes_together)
+
         return graph
 
-    def process_passes(self, raw_info: dict[dict]) -> float:
-        """Returns the assists of the player
+    def process_assists(self, raw_info: dict[dict]) -> tuple[int, dict[str, dict]]:
+        """Returns the assists, passes, and minutes together of the player in a dict
 
         Representation Invariant:
             - raw_info in same format as "passes_to" dict in LAL.json and DAL.json files
 
         """
-
+        pass_stats = {}
         assists = 0
         for player in raw_info:
             assists += raw_info[player]["assists"]
-        assists = assists
+            pass_stats[player] = raw_info[player]
 
-        return assists
+        return (assists, pass_stats)
 
     def process_defense(self, raw_info: dict[str, int]) -> dict:
         """Returns the points, rebounds, blocks, and steals alongside total minutes in a dict.
@@ -64,8 +76,7 @@ class LineupSimulation:
                 Representation Invariant:
                     - raw_info in same format as "defensive_stats" dict in LAL.json and DAL.json files
 
-                """
-
+        """
         minutes = raw_info['minutes']
         points = raw_info['points']
         rebounds = raw_info['rebounds']
@@ -75,3 +86,14 @@ class LineupSimulation:
         defense = {'points': points, 'rebounds': rebounds, 'steals': steals, 'blocks': blocks, 'minutes': minutes}
 
         return defense
+
+    """def get_player(self, player_name: str) -> Optional[_Player]:
+        
+        Returns the player object in self.players from the give player_name
+
+        
+        for player in self.players:
+            if player == player_name:
+                return self.players[player][0]
+        return None
+    """
